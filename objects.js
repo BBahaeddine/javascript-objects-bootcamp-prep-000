91fa67d6cac356b5308b06e6a4ba604ec4e4708e
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-02-28 19:23:02 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -8,4 +8,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
   // playlist['songTitle'] = songTitle;
   playlist.artistName = artisatName;
   playlist.songTitle = songTitle;
+  
+  return playlist;
 }
